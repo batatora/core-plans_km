1b8472d44247a80717b9eee167f17fc58a0dc831
simba-mupasa <simbamupasa@yandex.com>
2020-04-22 09:29:42 -0700

    Fix Travis on auto branch

--- travis.sh
+++ travis.sh
@@ -2,11 +2,21 @@
 # Script to run when testing core-plans on Travis CI
 set -x
 
+# debug info
+echo "TRAVIS_COMMIT=$TRAVIS_COMMIT"
+echo "TRAVIS_COMMIT_RANGE=$TRAVIS_COMMIT_RANGE"
+echo "HEAD=$(git rev-parse HEAD)"
+
+PARENT_COMMIT=$(git log --pretty=%P -n 1 "$TRAVIS_COMMIT")
+echo "PARENT_COMMIT=$PARENT_COMMIT"
+## end debug
+
 pre-commit install
 
 # Retrieve the canonical range.
 # The TRAVIS_COMMIT env var is always a merge commit, so we want to test from
 # the original commit all the way up until HEAD.
 ORIGIN=$(echo "$TRAVIS_COMMIT_RANGE" | cut -f1 -d'.')
+SOURCE=$(echo "$TRAVIS_COMMIT_RANGE" | cut -f4 -d'.')
 
-pre-commit run --origin "$ORIGIN" --source HEAD
+pre-commit run --origin "$ORIGIN" --source "$SOURCE"
