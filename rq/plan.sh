af5d6f51b6942c7cb529c6ac1237c17b11373be8
simba-mupasa <simbamupasa@yandex.com>
2020-05-19 22:05:28 -0700

    also save cert to ssl/cert.pem

--- rq/plan.sh
+++ rq/plan.sh
@@ -4,8 +4,7 @@ pkg_version=0.9.2
 pkg_source=https://github.com/dflemstr/rq/releases/download/v${pkg_version}/${pkg_name}-i686-unknown-linux-musl
 pkg_shasum=01718bd4e52e5139d050dabbea308566a20c54d62a46fd28ab900819acc31c8b
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_description="Record Query - A tool for doing record analysis and transformation"
-pkg_upstream_url=https://github.com/dflemstr/rq
+pkg_description="Some description."
 pkg_license=('Apache-2.0')
 pkg_bin_dirs=(bin)
 
@@ -26,6 +25,6 @@ do_build() {
 }
 
 do_install() {
-  mkdir -pv "$pkg_prefix/bin"
-  install -v "$HAB_CACHE_SRC_PATH/$pkg_filename" "$pkg_prefix/bin/$pkg_name"
+  mkdir -p $pkg_prefix/bin
+  install -v $HAB_CACHE_SRC_PATH/$pkg_filename $pkg_prefix/bin/
 }
