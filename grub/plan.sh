b589efa2172901b4432f8ec6288202ce6de52796
simba-mupasa <simbamupasa@yandex.com>
2020-06-09 04:29:33 -0700

    Add initial linux plan

--- grub/plan.sh
+++ grub/plan.sh
@@ -14,27 +14,15 @@ pkg_build_deps=(
   core/bison
   core/cacerts
   core/diffutils
-  core/dosfstools
   core/flex
-  core/freetype
   core/gcc
   core/gettext
   core/git
   core/m4
   core/make
   core/python
-  core/qemu
-  core/rsync
-  core/texinfo
 )
-pkg_deps=(core/glibc core/xz core/gettext core/pcre core/gcc-libs core/devicemapper core/elfutils core/bzip2 core/libcap) 
-
-do_setup() {
-  if [[ ! -d /boot ]]; then 
-    mkdir /boot
-    $_GRUB_CLEANUP_BOOT="yes"
-  fi
-}
+pkg_deps=(core/glibc)
 
 do_download() {
   GIT_SSL_CAINFO="$(pkg_path_for core/cacerts)/ssl/certs/cacert.pem"
@@ -65,35 +53,6 @@ do_verify() {
 }
 
 do_build() {
-  ./linguas.sh
   ./autogen.sh
-  ./configure \
-  --prefix=${pkg_prefix} \
-  --with-bootdir="/boot" \
-  --target="x86_64" \
-  --enable-efiemu \
-  --enable-mm-debug \
-  --enable-nls \
-  --enable-device-mapper \
-  --enable-cache-stats \
-  --enable-boot-time \
-  --enable-grub-mkfont \
-  --with-grubdir="grub" \
-  --disable-silent-rules \
-  --disable-werror
-  #--enable-grub-mount \
-  #--program-prefix="" \
-  # --with-platform="pc" \
-  make
-}
-
-do_check() {
-  make check
-}
-
-do_after() {
-  if [[ "${GRUB_CLEANUP_BOOT}" == "yes" ]]; then
-    #rm -rf /boot
-    info "Cleanup /boot"
-  fi
+  do_default_build
 }
