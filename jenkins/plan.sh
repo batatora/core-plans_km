a3edd94d769a716f7795eef751808f04d70fe227
simba-mupasa <simbamupasa@yandex.com>
2020-07-23 03:08:30 -0700

    Updated to 5.8.0

--- jenkins/plan.sh
+++ jenkins/plan.sh
@@ -1,22 +1,18 @@
 pkg_name=jenkins
 pkg_origin=core
-pkg_version=2.46.1
+pkg_version=2.89.1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="The leading open source automation server, Jenkins provides hundreds of plugins to support building, deploying and automating any project."
 pkg_license=('mit')
 pkg_upstream_url="https://jenkins.io/"
 pkg_source="http://mirrors.jenkins.io/war-stable/${pkg_version}/jenkins.war"
-pkg_shasum="33a3f4d983c6188a332291e1d974afa0a2ee96a0ae3cb6dd4f2098086525f9f1"
+pkg_shasum="f9f363959042fce1615ada81ae812e08d79075218c398ed28e68e1302c4b272f"
 pkg_deps=(core/jre8 core/curl)
 pkg_exports=(
     [port]=jenkins.http.port
 )
 pkg_exposes=(port)
 pkg_svc_user="root"
-
-do_clean() {
-    return 0
-}
 
 do_unpack() {
     return 0
