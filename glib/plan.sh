988b86f907206a885973f96613b0693d803516da
simba-mupasa <simbamupasa@yandex.com>
2020-07-24 00:44:15 -0700

    [traefik] bump 1.4.5 -> 1.4.6

--- glib/plan.sh
+++ glib/plan.sh
@@ -1,6 +1,6 @@
 pkg_origin=core
 pkg_name=glib
-pkg_version='2.50.1'
+pkg_version="2.50.3"
 pkg_description="$(cat << EOF
   GLib is a general-purpose utility library, which provides many useful data
   types, macros, type conversions, string utilities, file utilities, a
@@ -8,38 +8,61 @@ pkg_description="$(cat << EOF
   well as Windows and OS X.
 EOF
 )"
-pkg_source="http://download.gnome.org/sources/glib/$(echo $pkg_version | cut -d. -f1-2)/$pkg_name-$pkg_version.tar.xz"
+pkg_source="https://download.gnome.org/sources/${pkg_name}/${pkg_version%.*}/${pkg_name}-${pkg_version}.tar.xz"
 pkg_license=('LGPL-2.0')
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
-pkg_upstream_url="https://developer.gnome.org/glib/stable/glib.html"
-pkg_shasum="2ef87a78f37c1eb5b95f4cc95efd5b66f69afad9c9c0899918d04659cf6df7dd"
+pkg_upstream_url="https://developer.gnome.org/glib/"
+pkg_shasum="82ee94bf4c01459b6b00cb9db0545c2237921e3060c0b74cff13fbc020cfd999"
 pkg_deps=(
+  core/coreutils
+  core/elfutils
+  core/glibc
   core/libffi
   core/libiconv
   core/pcre
+  core/util-linux
+  core/zlib
 )
 pkg_build_deps=(
-  core/make
-  core/glibc
-  core/pkg-config
+  core/dbus
+  core/diffutils
+  core/file
   core/gcc
   core/gettext
-  core/zlib
+  core/libxslt
+  core/make
+  core/perl
+  core/pkg-config
   core/python
-  core/util-linux
 )
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 pkg_pconfig_dirs=(lib/pkgconfig)
+pkg_interpreters=(core/coreutils)
+
+do_prepare() {
+  if [[ ! -r /usr/bin/file ]]; then
+    ln -sv "$(pkg_path_for file)/bin/file" /usr/bin/file
+    _clean_file=true
+  fi
+}
 
 do_build() {
   ./configure \
     --prefix="$pkg_prefix" \
     --with-libiconv \
-    --with-pcre=system \
-    --disable-fam \
     --disable-gtk-doc \
-    --enable-shared
+    --disable-fam
   make
+}
+
+do_after() {
+  fix_interpreter "$pkg_prefix/bin/*" core/coreutils bin/env
+}
+
+do_end() {
+  if [[ -n "$_clean_file" ]]; then
+    rm -fv /usr/bin/file
+  fi
 }
--- traefik/plan.sh
+++ traefik/plan.sh
@@ -2,12 +2,15 @@ pkg_name=traefik
 pkg_description="a modern reverse proxy"
 pkg_upstream_url="https://traefik.io"
 pkg_origin=core
-pkg_version="v1.3.6"
+# note: to have the version match the codename, please update both values when
+#       updating this for a new release
+pkg_version="v1.4.6"
+traefik_codename="roquefort"
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=("MIT")
 pkg_source="http://github.com/containous/traefik"
 pkg_build_deps=(
-  core/node
+  core/node6
   core/sed
   core/yarn
 )
@@ -29,6 +32,11 @@ do_prepare() {
   build_line "adding \$GOPATH/bin to \$PATH"
   export PATH=${scaffolding_go_gopath:?}/bin:$PATH
 
+  build_line "setting \$VERSION to \$pkg_version"
+  export VERSION=$pkg_version
+  build_line "setting \$CODENAME to $traefik_codename"
+  export CODENAME=$traefik_codename
+
   build_line "building go-bindata"
   go get github.com/jteeuwen/go-bindata
   go install github.com/jteeuwen/go-bindata/...
@@ -46,17 +54,24 @@ do_download() {
 }
 
 do_build() {
+  # Note (2018/01/08): yarn uses core/node; traefik's build process depends on
+  # node-sass, which needs node6. So, we ensure that this ends up picking up
+  # the right node version for traefik to build.
+  # An alternative way would have been to change the order of dependencies in
+  # pkg_deps, but this is too brittle.
+  PATH=$(pkg_path_for core/node6)/bin:${PATH}
+  export PATH
   pushd "${scaffolding_go_gopath:?}/src/github.com/containous/traefik"
     build_line "building webui static assets"
     pushd webui
       yarn install
 
-      # We can't use `fix_interpreter` as core/node is not a runtime dep
+      # We can't use `fix_interpreter` as core/node6 is not a runtime dep
       for t in node_modules/.bin/*; do
         local interpreter_old
         local interpreter_new
         interpreter_old=".*node"
-        interpreter_new="$(pkg_path_for core/node)/bin/node"
+        interpreter_new="$(pkg_path_for core/node6)/bin/node"
         t="$(readlink --canonicalize --no-newline "$t")"
         build_line "Replacing '${interpreter_old}' with '${interpreter_new}' in '${t}'"
         sed -e "s#\#\!${interpreter_old}#\#\!${interpreter_new}#" -i "$t"
