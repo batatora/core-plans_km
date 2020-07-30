53baedcf9e89f5fc6d9a7b252a519fd01170a248
simba-mupasa <simbamupasa@yandex.com>
2020-07-30 03:52:53 -0700

    added README's to img and runc

--- vault/plan.sh
+++ vault/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=vault
-pkg_version=0.9.5
+pkg_version=0.9.1
 pkg_description="A tool for managing secrets."
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=("MPL-2.0")
 pkg_upstream_url=https://www.vaultproject.io/
 pkg_source=https://releases.hashicorp.com/vault/${pkg_version}/vault_${pkg_version}_linux_amd64.zip
-pkg_shasum=f6dbc9fdac00598d2a319c9b744b85bf17d9530298f93d29ef2065bc751df099
+pkg_shasum=6308013ee0d6278e98cdfe8d6de0162102a8d25f3bcd1e3737bf7b022a9f6702
 pkg_filename=${pkg_name}-${pkg_version}_linux_amd64.zip
 pkg_deps=()
 pkg_build_deps=(core/unzip)
