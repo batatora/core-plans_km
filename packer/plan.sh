649a21465749b112cea6bc4b28188c056fa813d3
simba-mupasa <simbamupasa@yandex.com>
2020-08-10 11:41:34 -0700

    breakout llvm components and upgrade to 5.0.1

--- packer/plan.sh
+++ packer/plan.sh
@@ -1,18 +1,19 @@
 pkg_name=packer
 pkg_origin=core
-pkg_version=1.2.1
+pkg_version=0.12.2
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MPL2')
 pkg_source=https://releases.hashicorp.com/${pkg_name}/${pkg_version}/${pkg_name}_${pkg_version}_linux_amd64.zip
-pkg_shasum=dd90f00b69c4d8f88a8d657fff0bb909c77ebb998afd1f77da110bc05e2ed9c3
+pkg_shasum=035d0ea1fe785ab6b673bc2a79399125d4014f29151e106635fa818bb726bebf
 pkg_description="Packer is a tool for creating machine and container images for multiple platforms from a single source configuration."
 pkg_upstream_url=packer.io
-pkg_build_deps=(core/unzip)
+pkg_dirname=$pkg_name
 pkg_bin_dirs=(bin)
 
 do_unpack() {
-  cd "${HAB_CACHE_SRC_PATH}" || exit
-  unzip "${pkg_filename}" -d "${pkg_name}-${pkg_version}"
+  pushd "${HAB_CACHE_SRC_PATH}"
+    unzip "${pkg_filename}" -d "packer"
+  popd
 }
 
 do_build() {
@@ -20,5 +21,5 @@ do_build() {
 }
 
 do_install() {
-  install -D packer "$pkg_prefix/bin/packer"
+  install -v "packer" "$pkg_prefix/bin/"
 }
