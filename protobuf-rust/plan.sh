96720aa5164ff658b7e34e26cac49f4bcc84c275
simba-mupasa <simbamupasa@yandex.com>
2020-08-13 05:04:20 -0700

    [libtasn1] Creates package

--- protobuf-rust/plan.sh
+++ protobuf-rust/plan.sh
@@ -7,16 +7,12 @@ pkg_bin_dirs=(bin)
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_upstream_url="https://github.com/stepancheg/rust-protobuf"
 pkg_description="Rust implementation of Google protocol buffers"
-pkg_deps=(
-  core/glibc
-  core/gcc-libs
-)
-pkg_build_deps=(
-  core/rust
-)
+pkg_deps=(core/glibc core/gcc)
+pkg_build_deps=(core/rust core/gcc core/cacerts core/protobuf-cpp)
 
 do_build() {
-  cargo install protobuf --root "${pkg_prefix}" --vers "${pkg_version}" -j"$(nproc)" --verbose
+  env SSL_CERT_FILE="$(pkg_path_for cacerts)/ssl/cert.pem" \
+    cargo install protobuf --root "${pkg_prefix}" --vers "${pkg_version}" -j"$(nproc)" --verbose
 }
 
 do_install() {
