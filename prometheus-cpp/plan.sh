4a4a8abfbf651c3a01c3f1e3a7ee214594b18ef2
simba-mupasa <simbamupasa@yandex.com>
2020-08-22 17:01:19 -0700

    [fontconfig] Update to 2.11.95

--- prometheus-cpp/plan.sh
+++ prometheus-cpp/plan.sh
@@ -1,22 +1,26 @@
 pkg_name=prometheus-cpp
 pkg_origin=core
-pkg_version=0.4.1
+git_sha=357e040a35204cee9d3ca48332aead9d56776f6e
+pkg_version="0.3-${git_sha:0:7}"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_upstream_url="https://github.com/jupp0r/prometheus-cpp"
+pkg_upstream_url=https://github.com/jupp0r/prometheus-cpp
 pkg_description="Prometheus Client Library for Modern C++"
-pkg_license=('MIT')
+pkg_licenses=('MIT')
 pkg_source="https://github.com/jupp0r/prometheus-cpp.git"
 pkg_shasum=noshasum
 pkg_deps=(
+  core/glibc
+  core/gcc-libs
+  core/protobuf
+  core/zlib
 )
 pkg_build_deps=(
   core/cacerts
-  core/cmake
   core/gcc
-  core/git
-  core/glibc
+  core/cmake
   core/make
-  core/zlib
+  core/git
+  core/telegraf
 )
 
 pkg_include_dirs=(include)
@@ -28,25 +32,23 @@ do_download() {
   GIT_SSL_CAINFO="$(pkg_path_for core/cacerts)/ssl/certs/cacert.pem"
   export GIT_SSL_CAINFO
 
-  REPO_PATH="$HAB_CACHE_SRC_PATH/$pkg_dirname"
+  cd "$HAB_CACHE_SRC_PATH" > /dev/null || exit
 
   # removing any previous git repo under the same package name that was downloaded
-  rm -rf "$REPO_PATH"
+  rm -rf "$pkg_name"
 
-  git clone "$pkg_source" "$REPO_PATH"
+  git clone "$pkg_source" "$pkg_name"
 
-  pushd "$REPO_PATH"
-  git checkout "tags/v${pkg_version}"
+  cd "$pkg_name" > /dev/null || exit
+  git reset --hard "$git_sha"
+  # git checkout "tags/v$pkg_version"
   git submodule init
   git submodule update
 }
 
-do_clean() {
-  return 0
-}
-
 do_unpack() {
-  return 0
+  cd "$HAB_CACHE_SRC_PATH" > /dev/null || exit
+  mv "$pkg_name" "$pkg_dirname"
 }
 
 do_verify() {
@@ -58,11 +60,14 @@ do_prepare() {
 }
 
 do_build() {
+  PROTOBUF_DIR="$(pkg_path_for core/protobuf)"
   ZLIB_DIR="$(pkg_path_for core/zlib)"
 
-  pushd "${BUILDDIR}"
+  cd "${BUILDDIR}" || exit
   cmake \
-    -DCMAKE_INSTALL_PREFIX="${pkg_prefix}" \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DProtobuf_INCLUDE_DIR="$PROTOBUF_DIR/include" \
+    -DProtobuf_LIBRARY="$PROTOBUF_DIR/lib/libprotobuf.so" \
     -DZLIB_INCLUDE_DIR="$ZLIB_DIR/include" \
     -DZLIB_LIBRARY="$ZLIB_DIR/lib/libz.so" \
     ..
@@ -71,11 +76,11 @@ do_build() {
 }
 
 do_check() {
-  pushd "${BUILDDIR}"
+  cd "${BUILDDIR}" || exit
   ctest -V
 }
 
 do_install() {
-  pushd "${BUILDDIR}"
+  cd "${BUILDDIR}" || exit
   make install
 }
