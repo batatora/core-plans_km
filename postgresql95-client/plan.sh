86d64bf5c34cc14199c1c5aa46c733c59d47f59e
simba-mupasa <simbamupasa@yandex.com>
2020-09-23 07:40:16 -0700

    Version up to 1_66_0

--- postgresql95-client/plan.sh
+++ postgresql95-client/plan.sh
@@ -1,15 +1,15 @@
 source ../postgresql95/plan.sh
 
 pkg_name=postgresql95-client
-# Default to version/shasum from sourced postgresql95 plan
-pkg_version=${pkg_version:-9.5.13}
+# Default to version/shasum from sourced postgres94 plan
+pkg_version=${pkg_version:-9.5.6}
 pkg_origin=core
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="PostgreSQL is a powerful, open source object-relational database system."
 pkg_upstream_url="https://www.postgresql.org/"
 pkg_license=('PostgreSQL')
-pkg_source="https://ftp.postgresql.org/pub/source/v${pkg_version}/postgresql-${pkg_version}.tar.bz2"
-pkg_shasum="${pkg_shasum:-5408b86a0b56fd0140c6a0016bf9179bc7817fa03d5571cca346c9ab122ea5ee}"
+pkg_source=https://ftp.postgresql.org/pub/source/v${pkg_version}/postgresql-${pkg_version}.tar.bz2
+pkg_shasum=${pkg_shasum:-bb9e5f6d34e20783e96e10c1d6c0c09c31749e802aaa46b793ce2522725ae12f}
 pkg_dirname="postgresql-${pkg_version}"
 
 # No exports/exposes for client
