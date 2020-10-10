07887ba170a9ed7257cf5a32a47fed3aeb4b0335
simba-mupasa <simbamupasa@yandex.com>
2020-10-09 22:59:18 -0700

    Upgrade re2c to 1.1.1, update readme, add tests

--- re2c/test.sh
+++ re2c/test.sh
@@ -4,12 +4,6 @@ SKIPBUILD=${SKIPBUILD:-0}
 
 hab pkg install --binlink core/bats
 
-hab pkg install core/busybox-static
-hab pkg binlink core/busybox-static ps
-hab pkg binlink core/busybox-static netstat
-hab pkg binlink core/busybox-static wc
-hab pkg binlink core/busybox-static uniq
-
 source ./plan.sh
 
 if [ "${SKIPBUILD}" -eq 0 ]; then
@@ -17,7 +11,6 @@ if [ "${SKIPBUILD}" -eq 0 ]; then
   build
   source results/last_build.env
   hab pkg install --binlink --force "results/${pkg_artifact}"
-  hab svc load "${pkg_ident}"
   set +e
 
   # Give some time for the service to start up
