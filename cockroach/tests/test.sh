45bf90ff52c56d8a08c61ab303bdefae6e9a134c
simba-mupasa <simbamupasa@yandex.com>
2020-10-24 01:05:52 -0700

    Add cppcheck

--- cockroach/tests/test.sh
+++ cockroach/tests/test.sh
@@ -1,31 +1,34 @@
 #!/bin/sh
+
 TESTDIR="$(dirname "${0}")"
 PLANDIR="$(dirname "${TESTDIR}")"
 SKIPBUILD=${SKIPBUILD:-0}
 
-hab pkg install core/busybox-static
-hab pkg binlink core/busybox-static netstat
-
 hab pkg install --binlink core/bats
+
+hab pkg install core/busybox-static
+hab pkg binlink core/busybox-static ps
+hab pkg binlink core/busybox-static netstat
+hab pkg binlink core/busybox-static wc
+hab pkg binlink core/busybox-static uniq
+
 source "${PLANDIR}/plan.sh"
 
 if [ "${SKIPBUILD}" -eq 0 ]; then
+  # Unload the service if its already loaded.
+  hab svc unload "${HAB_ORIGIN}/${pkg_name}"
+
   set -e
   pushd "${PLANDIR}" > /dev/null
   build
   source results/last_build.env
-  hab svc load -f core/nginx
-  echo '[http.listen]
-port = 81' | hab config apply nginx.default "$(date +%s)"
-  # Unload the service if its already loaded.
-  hab svc unload "${HAB_ORIGIN}/${pkg_name}"
   hab pkg install --binlink --force "results/${pkg_artifact}"
-  hab svc load "${pkg_ident}" --bind=backend:nginx.default
+  hab svc load "${pkg_ident}"
   popd > /dev/null
   set +e
-fi
 
-echo "Waiting 5 seconds hoping haproxy will start..."
-sleep 5
+  # Give some time for the service to start up
+  sleep 3
+fi
 
 bats "${TESTDIR}/test.bats"
