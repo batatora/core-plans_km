45af8fbac18af58e188e94228a4e7c5f2fb49e0e
simba-mupasa <simbamupasa@yandex.com>
2020-10-29 06:30:21 -0700

    Update terraform to 0.11.10

--- benchmark/plan.sh
+++ benchmark/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=benchmark
 pkg_origin=core
-pkg_version=1.4.1
+pkg_version=1.4.0
 pkg_description="Google's microbenchmark support library"
 pkg_upstream_url=https://github.com/google/benchmark
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://github.com/google/benchmark/archive/v${pkg_version}.tar.gz"
 pkg_filename="v${pkg_version}.tar.gz"
-pkg_shasum=f8e525db3c42efc9c7f3bc5176a8fa893a9a9920bbd08cef30fb56a51854d60d
+pkg_shasum=616f252f37d61b15037e3c2ef956905baf9c9eecfeab400cb3ad25bae714e214
 pkg_deps=(
   core/gcc-libs
   core/glibc
@@ -19,52 +19,37 @@ pkg_build_deps=(
   core/gcc
   core/git
   core/googletest
+  core/googlemock
 )
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 
-do_begin() {
-  export HAB_ENV_CMAKE_FIND_ROOT_PATH_SEPARATOR=";"
-  export HAB_ENV_CMAKE_FIND_ROOT_PATH_TYPE="aggregate"
-}
-
-do_setup_environment() {
-  set_buildtime_env BUILD_DIR "_build"
-
-  # this allows cmake users to utilize `CMAKE_FIND_ROOT_PATH` to find various cmake configs
-  push_runtime_env CMAKE_FIND_ROOT_PATH "${pkg_prefix}/lib/cmake/benchmark"
-}
+BUILDDIR="build"
 
 do_prepare() {
-  mkdir -p "${BUILD_DIR}"
+  mkdir -p "$BUILDDIR"
 }
 
 do_build() {
   GTEST_DIR="$(pkg_path_for core/googletest)"
 
-  pushd "${BUILD_DIR}" || exit 1
-
+  cd "$BUILDDIR" || exit
   cmake \
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-    -DBENCHMARK_ENABLE_TESTING="${DO_CHECK}" \
-    -DBENCHMARK_ENABLE_GTEST_TESTS="${DO_CHECK}" \
-    -DGTEST_LIBRARY="${GTEST_DIR}/lib64/libgtest.a" \
-    -DGTEST_MAIN_LIBRARY="${GTEST_DIR}/lib64/libgtest_main.a" \
-    -DGTEST_INCLUDE_DIR="${GTEST_DIR}/include" \
+    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
     -DCMAKE_BUILD_TYPE="RELEASE" \
+    -DGTEST_LIBRARY="$GTEST_DIR/lib/libgtest.a" \
+    -DGTEST_MAIN_LIBRARY="$GTEST_DIR/lib/libgtest_main.a" \
+    -DGTEST_INCLUDE_DIR="$GTEST_DIR/include" \
     ..
-  make -j"$(nproc --ignore=1)"
-  popd || exit 1
+  make
 }
 
 do_check() {
-  pushd "${BUILD_DIR}" || exit 1
-  make test -j"$(nproc --ignore=1)"
-  popd || exit 1
+  cd "$BUILDDIR" || exit
+  make test
 }
 
 do_install() {
-  pushd "${BUILD_DIR}" || exit 1
+  cd "$BUILDDIR" || exit
   make install
-  popd || exit 1
 }
