a685f63c46188fdf1e526cc1475f363f96d65dc2
simba-mupasa <simbamupasa@yandex.com>
2020-11-11 03:15:08 -0800

    Add libzip support validation test

--- ansible/plan.sh
+++ ansible/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=ansible
 pkg_origin=core
-pkg_version="2.7.1"
+pkg_version="2.4.3.0-1"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("GPL-3.0")
 pkg_source="https://github.com/${pkg_name}/${pkg_name}/archive/v${pkg_version}.tar.gz"
 pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum="354f3fcf5fa837d48a8cd9bff7c74db0c85a2031d608109481c467a7d57cc3b9"
+pkg_shasum="0bcb583f2abaa580c337462a81903ff3f925d10c367da466c2c0354479432683"
 pkg_deps=(
   core/libffi
   core/python2
@@ -18,7 +18,10 @@ pkg_build_deps=(
   core/openssl
 )
 pkg_bin_dirs=(bin)
-pkg_description="Ansible is a radically simple IT automation platform that makes your applications and systems easier to deploy."
+pkg_description="Ansible is a radically simple IT automation platform that makes your applications \
+and systems easier to deploy. Avoid writing scripts or custom code to deploy and update your \
+applications— automate in a language that approaches plain English, using SSH, with no agents to \
+install on remote systems."
 pkg_upstream_url="https://www.ansible.com/"
 
 do_setup_environment() {
--- scaffolding-chef/lib/scaffolding.sh
+++ scaffolding-chef/lib/scaffolding.sh
@@ -52,12 +52,8 @@ export SSL_CERT_FILE="{{pkgPathFor "core/cacerts"}}/ssl/cert.pem"
 cd {{pkg.path}}
 
 exec 2>&1
-while true; do
-SPLAY_DURATION=\$({{pkgPathFor "core/coreutils"}}/bin/shuf -i 0-{{cfg.splay}} -n 1)
-sleep \$SPLAY_DURATION
-chef-client -z -l {{cfg.log_level}} -c $pkg_svc_config_path/client-config.rb --once --no-fork --run-lock-timeout {{cfg.run_lock_timeout}}
-sleep {{cfg.interval}}
-done
+chef-client -z -l {{cfg.log_level}} -c $pkg_svc_config_path/client-config.rb --once
+exec chef-client -z -i {{cfg.interval}} -s {{cfg.splay}} -l {{cfg.log_level}} -c $pkg_svc_config_path/client-config.rb
 EOF
   chown 0755 "$pkg_prefix/hooks/run"
 }
@@ -95,7 +91,7 @@ cache_path "$pkg_svc_data_path/cache"
 node_path "$pkg_svc_data_path/nodes"
 role_path "$pkg_svc_data_path/roles"
 
-ssl_verify_mode {{cfg.ssl_verify_mode}}
+ssl_verify_mode :verify_none
 chef_zero.enabled true
 EOF
 
@@ -119,10 +115,8 @@ EOF
   cat << EOF >> "$pkg_prefix/default.toml"
 interval = 1800
 splay = 180
-run_lock_timeout = 1800
 log_level = "warn"
 env_path_prefix = "/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin"
-ssl_verify_mode = ":verify_peer"
 
 [data_collector]
 enable = false
