11648035fd7e1937acf4a0bbe993733774d18eaf
simba-mupasa <simbamupasa@yandex.com>
2020-11-12 01:43:20 -0800

    Upgrade Jenkins to 2.138.3

--- node10/plan.ps1
+++ node10/plan.ps1
@@ -2,7 +2,7 @@
 
 $pkg_name="node10"
 $pkg_origin="core"
-$pkg_version="10.8.0"
+$pkg_version="10.11.0"
 $pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
 $pkg_source="https://nodejs.org/dist/v$pkg_version/node-v$pkg_version-x64.msi"
-$pkg_shasum="9d03d6bc78d7375fa549005c9b12cf5da4b01ee52b60834107f5f603d82a68f2"
+$pkg_shasum="46cc9fe075db65a081607a8c803d098deccbb0f1ab3b3dafb9651ec9c0ac853f"
