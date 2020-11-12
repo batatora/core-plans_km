32358a2b0e25bc7b890fdac85af54060d8629637
simba-mupasa <simbamupasa@yandex.com>
2020-11-11 18:05:50 -0800

    Convert to unix-style line endings

--- node11/plan.ps1
+++
@@ -1,8 +0,0 @@
-. "..\node\plan.ps1"
-
-$pkg_name="node11"
-$pkg_origin="core"
-$pkg_version="11.0.0"
-$pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
-$pkg_source="https://nodejs.org/dist/v$pkg_version/node-v$pkg_version-x64.msi"
-$pkg_shasum="a4be82fad7610131a68507aad93db4bb5809025af499b667e8201c93ee85aea4"
