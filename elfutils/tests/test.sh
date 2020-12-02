f90a44aadd684e970a780f5ae274c10355daafd8
simba-mupasa <simbamupasa@yandex.com>
2020-12-02 00:44:58 -0800

    Update Elixir to 1.8.0

--- elfutils/tests/test.sh
+++ elfutils/tests/test.sh
@@ -5,7 +5,6 @@ PLANDIR="$(dirname "${TESTDIR}")"
 SKIPBUILD=${SKIPBUILD:-0}
 
 hab pkg install --binlink core/bats
-hab pkg install --binlink "core/curl"
 
 source "${PLANDIR}/plan.sh"
 
@@ -19,10 +18,4 @@ if [ "${SKIPBUILD}" -eq 0 ]; then
   set +e
 fi
 
-hab svc load "${pkg_ident}"
-hab svc start "${pkg_ident}"
-sleep 120
-
 bats "${TESTDIR}/test.bats"
-
-hab svc unload "${pkg_ident}"
