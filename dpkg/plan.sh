6e228cd05468664d70297e8dca17c33abeb40ae7
simba-mupasa <simbamupasa@yandex.com>
2020-12-10 15:02:00 -0800

    Add usage to the ruby26 package

--- dpkg/plan.sh
+++ dpkg/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=dpkg
 pkg_origin=core
-pkg_version=1.19.4
+pkg_version=1.18.24
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('GPL-2.0')
 pkg_upstream_url=https://wiki.debian.org/dpkg
 pkg_description="dpkg is a package manager for Debian-based systems"
 pkg_source=http://http.debian.net/debian/pool/main/d/${pkg_name}/${pkg_name}_${pkg_version}.tar.xz
-pkg_shasum=c15234e98655689586bff2d517a6fdc6135d139c54d52ae9cfa6a90007fee0ae
+pkg_shasum=d853081d3e06bfd46a227056e591f094e42e78fa8a5793b0093bad30b710d7b4
 pkg_deps=(
   core/bzip2
   core/glibc
--- dpkg/tests/test.bats
+++
@@ -1,6 +0,0 @@
-source "${BATS_TEST_DIRNAME}/../plan.sh"
-
-@test "Version matches" {
-  result="$(dpkg --version | head -1 | awk '{print $7}')"
-  [ "$result" = "${pkg_version}" ]
-}
