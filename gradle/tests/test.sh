fef24afdeaedd9851da1142ce64d4735bca966d1
simba-mupasa <simbamupasa@yandex.com>
2020-12-12 11:07:11 -0800

    Updated gradle to latest version and split version 4 into gradle4.

--- gradle/tests/test.sh
+++ gradle/tests/test.sh
@@ -5,6 +5,7 @@ PLANDIR="$(dirname "${TESTDIR}")"
 SKIPBUILD=${SKIPBUILD:-0}
 
 hab pkg install --binlink core/bats
+hab pkg install --binlink --force "core/jre8"
 
 source "${PLANDIR}/plan.sh"
 
@@ -18,4 +19,9 @@ if [ "${SKIPBUILD}" -eq 0 ]; then
   set +e
 fi
 
+JAVA_HOME="$(hab pkg path core/jre8)"
+export JAVA_HOME
+
 bats "${TESTDIR}/test.bats"
+
+rm -rf "${PLANDIR}/../.gradle"
